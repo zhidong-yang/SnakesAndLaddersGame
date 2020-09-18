7e8053a7a9ffb7c2f6252a8e8f85642ed67288c6
zhidong-yang <zhidong.yang@outlook.com>
2020-09-18 16:34:23 -0400

    Built PlayerModel and Gameboard model

---
+++ SnakesAndLaddersGame/Libraries/Gameboard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLaddersGame.Libraries
+{
+    public class Gameboard
+    {
+        int[] SnakeHeads = { 16, 46, 49, 62, 64, 74, 89, 92, 95, 99 };
+        int[] LadderFoots = { 2, 7, 8, 15, 21, 28, 36, 51, 71, 78, 87 };
+
+        public int GameboardCells(int num)
+        {
+            Dictionary<int, int> cellStopAt = new Dictionary<int, int>();
+            if (SnakeHeads.Contains(num))
+            {
+                cellStopAt[16] = 6;
+                cellStopAt[46] = 25;
+                cellStopAt[49] = 11;
+                cellStopAt[62] = 19;
+                cellStopAt[64] = 60;
+                cellStopAt[74] = 53;
+                cellStopAt[89] = 68;
+                cellStopAt[92] = 88;
+                cellStopAt[95] = 75;
+                cellStopAt[99] = 80;
+            }
+            else if (LadderFoots.Contains(num))
+            {
+                cellStopAt[2] = 38;
+                cellStopAt[7] = 14;
+                cellStopAt[8] = 31;
+                cellStopAt[15] = 26;
+                cellStopAt[21] = 42;
+                cellStopAt[28] = 84;
+                cellStopAt[36] = 44;
+                cellStopAt[51] = 67;
+                cellStopAt[71] = 91;
+                cellStopAt[78] = 98;
+                cellStopAt[87] = 94;
+            }
+            else
+            {
+                cellStopAt[num] = num;
+            }
+
+            return cellStopAt[num];
+        }
+    }
+}
---
+++ SnakesAndLaddersGame/Libraries/PlayerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLaddersGame.Libraries
+{
+    public class PlayerModel
+    {
+        public string Name { get; set; }
+        
+        Random rdn = new Random();
+        public int[] RollDice()
+        {
+            int[] output = new int[2];
+            output[0] = rdn.Next(1, 7);
+            output[1] = rdn.Next(1, 7);
+
+            return output;
+        }
+    }
+}
